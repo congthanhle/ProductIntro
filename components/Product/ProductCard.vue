ed4ac3d6422c9ad7a8dd5f84dbb0b60b32bc7340
congthanhle <lecongthanhbaoloclamdong@gmail.com>
2023-10-24 09:29:54 +0700

    create delete, edit  feature

--- components/Product/ProductCard.vue
+++ components/Product/ProductCard.vue
@@ -24,7 +24,7 @@ const props = defineProps<{
 
 const handleDeleteProduct = async () => {
     await store.dispatch("DELETE_PRODUCT", props.product.id)
-    window.location.reload();
+
 }
 
 </script>
